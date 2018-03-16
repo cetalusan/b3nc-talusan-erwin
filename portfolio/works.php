bca5922bb2cd766a4395183d7313f094f635cfd9
cetalusan <33026234+cetalusan@users.noreply.github.com>
2018-03-16 21:42:03 +0800

    Add files via upload
    Portfolio

---
+++ portfolio/works.php
@@ -0,0 +1,49 @@
+<?php 
+
+include 'includes/head.php'
+
+ ?>
+
+</head>
+<body>
+	<header class="header-wrapper">
+		<div id="logo">
+			Logo
+		</div>
+
+		<nav id="top-nav">
+			<a href="about.php">About</a>
+			<a href="works.php">Works</a>
+			<a href="contact.php">Contact</a>
+		</nav>
+	</header>
+
+	<div class="land-message">
+		<h1>Hello There!</h1>
+	</div>
+
+	<div class="work" >
+		<div class="box">
+			
+		</div>
+
+		<div class="box">
+			
+		</div>
+
+		<div class="box">
+			
+		</div>
+	</div>
+	
+
+	<?php
+
+	include 'includes/foot.php';
+
+	?>
+
+
+
+</body>
+</html>
