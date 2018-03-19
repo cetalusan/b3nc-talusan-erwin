e171a3db211bfa2d11e1a7d8c39bc367159c06c9
cetalusan <33026234+cetalusan@users.noreply.github.com>
2018-03-19 18:49:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php 
+
+include 'includes/head.php'
+
+?>
+
+</head>
+<body>
+	<header class="header-wrapper">
+		<div id="logo">
+			<h2>Erwin Talusan</h2>
+		</div>
+
+		<nav id="top-nav">
+			<a href="about.php">About</a>
+			<a href="works.php">Works</a>
+			<a href="contact.php">Contact</a>
+		</nav>
+	</header>
+
+	<div class="land-message landing-page">
+		<h1>ERWIN TALUSAN</h1>
+		<h4>Web Developer</h4>
+	</div>
+
+	<div class="work" >
+		<div class="box">
+			<a href="https://cetalusan.github.io/b3nc-talusan-erwin/csp1-updated/index.html">
+				<div class="hoverBox">
+					<h3>Built Cycles</h3>
+					<h5>Web Design</h5>
+				</div>
+			</a>
+		</div>
+
+		<div class="box">
+			<a href="https://cetalusan.github.io/b3nc-talusan-erwin/csp1-updated/index.html">
+				<div class="hoverBox">
+					<h3>Built Cycles</h3>
+					<h5>Web Design</h5>
+				</div>
+			</a>
+		</div>
+
+		<div class="box">
+			<a href="https://cetalusan.github.io/b3nc-talusan-erwin/csp1-updated/index.html"></a>
+			<div class="hoverBox">
+				<h3>Built Cycles</h3>
+				<h5>Web Design</h5>
+			</div>
+			
+		</div>
+	</div>
+
+	<div class="about">
+
+		
+	</div>
+
+	<div class="contact">
+		
+	</div>
+	
+
+	<?php
+
+	include 'includes/foot.php';
+
+	?>
+
+
+
+</body>
+</html>
